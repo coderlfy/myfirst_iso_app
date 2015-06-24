d928c85ea6c72253dfeb41fd9b918e69d7dbd876
李峰艳 <a@rihouyandeMacBook-Air.local>
2015-06-24 16:29:25 +0800

    Initial Commit

---
+++ MyFirstApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyFirstApp
+//
+//  Created by 李峰艳 on 15/6/24.
+//  Copyright (c) 2015年 李峰艳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyFirstApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyFirstApp
+//
+//  Created by 李峰艳 on 15/6/24.
+//  Copyright (c) 2015年 李峰艳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
