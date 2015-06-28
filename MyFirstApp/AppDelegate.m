dd19faf797403c61d109675b949faa72a611771e
李峰艳 <a@rihouyandeMacBook-Air.local>
2015-06-28 12:35:47 +0800

    myfirstapp

--- MyFirstApp/AppDelegate.m
+++ MyFirstApp/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "MainController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    MainController *main = [[MainController alloc] init];
+    [self.window addSubview:[main view]];
+    //[main release];
     return YES;
 }
 
---
+++ MyFirstApp/controller/MainController.h
@@ -0,0 +1,15 @@
+//
+//  MainController.h
+//  MyFirstApp
+//
+//  Created by 李峰艳 on 15/6/28.
+//  Copyright (c) 2015年 李峰艳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainController : UITableViewController
+
+@property (nonatomic,retain) NSMutableArray *data;
+
+@end
